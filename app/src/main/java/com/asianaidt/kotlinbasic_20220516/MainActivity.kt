dfe7f46ee0143e85823d43a8b403657425ab62d5
전지혜 <jeonjh9535@naver.com>
2022-05-16 14:37:49 +0900

    로그/토스트 띄우기

--- app/src/main/java/com/asianaidt/kotlinbasic_20220516/MainActivity.kt
+++ app/src/main/java/com/asianaidt/kotlinbasic_20220516/MainActivity.kt
@@ -3,6 +3,7 @@ package com.asianaidt.kotlinbasic_20220516
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +16,20 @@ class MainActivity : AppCompatActivity() {
             Log.d("메인화면", "로그 찍기 버튼 눌림")
 
         }
+
+        btnLog.setOnLongClickListener{
+
+            Log.d("메인화면","로그 버튼이 길게 눌림")
+
+            return@setOnLongClickListener true
+        }
+
+        btnLog.setOnClickListener {
+
+            Toast.makeText(this,"테스트용 토스트",Toast.LENGTH_SHORT).show()
+
+        }
     }
+
+
 }
